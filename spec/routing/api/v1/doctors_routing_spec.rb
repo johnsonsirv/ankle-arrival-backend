1d0009877457e49415cd94b9e3edce35fdaaff69
johnsonsirv <johnsonsirv@gmail.com>
2020-08-06 14:07:34 +0000

    refac: cleanup empty spec files

--- spec/routing/api/v1/doctors_routing_spec.rb
+++ spec/routing/api/v1/doctors_routing_spec.rb
@@ -9,22 +9,6 @@ RSpec.describe Api::V1::DoctorsController, type: :routing do
     it "routes to #show" do
       expect(:get => "/api/v1/doctors/1").to route_to("api/v1/doctors#show", :id => "1")
     end
-
-
-    it "routes to #create" do
-      expect(:post => "/api/v1/doctors").to route_to("api/v1/doctors#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(:put => "/api/v1/doctors/1").to route_to("api/v1/doctors#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/api/v1/doctors/1").to route_to("api/v1/doctors#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/api/v1/doctors/1").to route_to("api/v1/doctors#destroy", :id => "1")
-    end
+   
   end
 end
